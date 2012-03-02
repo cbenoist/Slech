7d638d4b715990ed6de93b63db5a25a7f9efc29a
Cliff Benoist <cbenoist@gmail.com>
2012-03-01 22:41:40 -0500

    One model class was removed in the project setup mayhem.

---
+++ Slech/Models/SlechModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Slech.Models
+{
+    public class SlechModel
+    {
+        public int Id { get; set; }
+        public string Slech { get; set; }
+        public DateTime SubmitDate { get; set; }
+
+        public virtual SlecherModel Slecher { get; set; }
+    }
+}
